74e38c9e3fd17ee013db4a9597c6780e0361a87b
pbcquoc <pbcquoc@gmail.com>
2021-04-18 11:33:16 +0700

    insertion sort

---
+++ sort/recursive_insertion_sort.py
@@ -0,0 +1,20 @@
+"""
+"""
+def insertion_sort(arr, n):
+    if n == 1:
+        return 
+
+    insertion_sort(arr, n -1)
+    key = arr[n -1]
+    j = n - 2
+    while j >= 0 and key < arr[j]:
+        arr[j+1] = arr[j]
+        j -= 1
+
+    arr[j+1] = key
+
+
+arr = [12,11,13,5,6]
+
+insertion_sort(arr, len(arr))
+print(arr)
