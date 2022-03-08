4765db17de70cbf6e8bd8ccdc68bc534b2d69e0b
pbcquoc <pbcquoc@gmail.com>
2022-03-08 17:52:10 +0700

    radix sort

---
+++ sort/sort_n_number_n2.py
@@ -0,0 +1,27 @@
+def count_sort(arr, n, exp):
+    output = [0]*n
+    count = [0]*n
+
+    for i in range(n):
+        count[(arr[i]//exp) % n] += 1
+
+    for i in range(1, n):
+        count[i] += count[i-1]
+
+    for i in range(n-1, -1, -1):
+        output[count[(arr[i]//exp) %n] - 1] = arr[i]
+        count[(arr[i]//exp) %n] -= 1
+
+    for i in range(n):
+        arr[i] = output[i]
+
+
+def sort(arr, n):
+    count_sort(arr, n, 1)
+    count_sort(arr, n, n)
+
+arr = [40, 12, 45, 32, 33, 1, 22]
+n = len(arr)
+sort(arr, n)
+
+print(arr)
