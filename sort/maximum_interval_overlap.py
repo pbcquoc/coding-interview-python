8a354f84f7244637956bdf4268373f5b6393defd
pbcquoc <pbcquoc@gmail.com>
2022-03-16 21:30:51 +0700

    max guest

--- sort/maximum_interval_overlap.py
+++ sort/maximum_interval_overlap.py
@@ -35,6 +35,27 @@ def max_guest(arrl, exit, n):
     
     return time, max_guests
 
+def max_guest(arrl, exit, n):
+    maxi =max(max(arrl), max(exit))
+    cur = 0
+    idx = 0
+    x = [0]*(maxi+2)
+
+    for i in range(n):
+        x[arrl[i]] += 1
+        x[exit[i]+1] -= 1
+    
+    print(x)
+    max_gst = 0
+    for i in range(maxi+1):
+        cur += x[i]
+        if cur > max_gst:
+            max_gst = cur
+            idx = i
+
+    print(idx, max_gst)
+
+
 arrl = [1, 2, 10, 5, 5];
 exit = [4, 5, 12, 9, 12];
 
