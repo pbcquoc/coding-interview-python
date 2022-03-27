b0c66ea26841aae30f15510886c33b29bfb4a332
pbcquoc <pbcquoc@gmail.com>
2022-03-27 15:56:36 +0700

    approach insertion sort

--- sort/rearrange_pos_neg.py
+++ sort/rearrange_pos_neg.py
@@ -4,7 +4,7 @@ def rotate(arr, l, r):
         arr[j] = arr[j-1]
     arr[l] = tmp
 
-def move_negative(arr, n):
+def move_negative_qsort(arr, n):
     i = -1
     for j in range(n):
         if arr[j] < 0:
@@ -14,6 +14,24 @@ def move_negative(arr, n):
             if j - i >= 2:
                 rotate(arr, i+1, j)
 
+def move_negative_insertion_sort(arr, n):
+    for i in range(1, n):
+        key = arr[i]
+        if key >= 0:
+             continue 
+
+        j = i - 1
+        while j >= 0 and arr[j] >= 0:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+    
+
+            
+
+
+
 arr = [5, 5, -3, 4, -8, 0, -7, 3, -9, -3, 9, -2, 1]
-move_negative(arr, len(arr))
+move_negative_insertion_sort(arr, len(arr))
 print(arr)
