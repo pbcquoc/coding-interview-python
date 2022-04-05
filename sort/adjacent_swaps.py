8aaf1ec740b2f764519270c689b886b72cc727de
pbcquoc <pbcquoc@gmail.com>
2022-04-05 21:24:50 +0700

    adjacent swaps

--- sort/adjacent_swaps.py
+++ sort/adjacent_swaps.py
@@ -2,7 +2,6 @@
 https://www.geeksforgeeks.org/number-swaps-sort-adjacent-swapping-allowed/
 """
 def merge(arr, tmp, left, mid, right):
-    
     i = left
     j = mid
     k = left
@@ -20,4 +19,38 @@ def merge(arr, tmp, left, mid, right):
             
             inv_count += (mid - i)
 
+    while i <= mid - 1:
+        tmp[k] = arr[i]
+        k += 1
+        i += 1
+
+    while j <= right:
+        tmp[k] = arr[j]
+        k += 1
+        j += 1
+
+
+    for i in range(left, right+1):
+        arr[i] = tmp[i]
+
+    return inv_count
+
+def merge_sort(arr, tmp, left, right):
+    inv_count = 0
+    if left < right:
+        mid = (left + right)//2
+
+        inv_count += merge_sort(arr, tmp, left, mid)
+        inv_count += merge_sort(arr, tmp, mid+1, right)
+        inv_count += merge(arr, tmp, left, mid+1, right)
+
+    return inv_count
+
+def inv_count(arr, n):
+    tmp = [0]*n
+    
+    return merge_sort(arr, tmp, 0, n -1)
+
+arr = [1, 20, 6, 4, 5]
+print(inv_count(arr, len(arr)))
 
