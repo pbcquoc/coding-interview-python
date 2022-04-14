109b04fcc87512410a297e3a75cb698eb3904c49
pbcquoc <pbcquoc@gmail.com>
2022-04-14 15:56:11 +0700

    reversing subarray

--- sort/reversing_subarray.py
+++ sort/reversing_subarray.py
@@ -19,5 +19,35 @@ def check_reverse(arr, n):
 
     return True
 
-arr = [1, 2, 5, 4, 3]
+def check_reverse(arr, n):
+    if n == 1:
+        return True
+
+    i = 1
+    for i in range(1, n):
+        if arr[i -1] < arr[i]:
+            if i == n:
+                return True
+        else:
+            break
+    print(i) 
+    j = i
+    while j < n and arr[j] < arr[j-1]:
+        if i > 1 and arr[j] < arr[i-2]:
+            return False
+        j+=1
+        if j==n:
+            return True
+    print(i, j)
+    k = j
+    if arr[k] < arr[i-1]:
+        return False
+
+    while k > 1 and k < n:
+        if arr[k] < arr[k -1]:
+            return False
+        k += 1
+
+    return True
+arr = [1,2,3,4,20,9,16,17]
 print(check_reverse(arr, len(arr)))
