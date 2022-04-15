b195d43b8b30a157e139ad70cf17a74fc5f3b937
pbcquoc <pbcquoc@gmail.com>
2022-04-15 21:43:17 +0700

    sort square

--- sort/sort_square_array.py
+++ sort/sort_square_array.py
@@ -32,7 +32,26 @@ def sort_square(arr, n):
     for i in range(n):
         arr[i] = tmp[i]
 
-    
+def sort_square(arr, n):
+    left = 0
+    right = n - 1
+    index = n - 1
+    result = [0]*n
+
+    while index >= 0:
+        if abs(arr[left]) >= abs(arr[right]):
+            result[index] = arr[left]*arr[left]
+            left += 1
+        else:
+            result[index] = arr[right]*arr[right]
+            right -= 1
+        index -= 1
+
+    for i in range(n):
+        arr[i] = result[i]
+
+    return arr
+
 arr = [-6, -3, -1, 2, 4, 5 ]
 sort_square(arr, len(arr))
 print(arr)
