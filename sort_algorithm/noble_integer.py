67eb788699632812fae3d97feb232e7506a49215
pbcquoc <pbcquoc@gmail.com>
2022-04-23 22:48:16 +0700

    noble integer

--- sort_algorithm/noble_integer.py
+++ sort_algorithm/noble_integer.py
@@ -13,5 +13,27 @@ def noble_integer(arr, n):
     
     return -1
 
+def noble_integer(arr, n):
+    count = [0]*(n+1)
+
+    for i in range(n):
+        if arr[i] < 0:
+            continue
+        elif arr[i]>=n:
+            count[n] += 1
+        else:
+            count[arr[i]] += 1
+    
+    total_greater = count[n]
+    for i in range(n-1, -1, -1):
+        if total_greater == i and count[i] > 0:
+            return i
+        
+        elif total_greater > i:
+            return -1
+        total_greater += count[i]
+
+    return -1
+
 arr = [10, 3, 20, 40, 2]
 print(noble_integer(arr, len(arr)))
