c1d9266be866dfcedd5fd89e5113c106ba8bbec3
pbcquoc <pbcquoc@gmail.com>
2022-04-24 18:41:48 +0700

    smallest diff pair

--- sort_algorithm/smallest_diff_pair.py
+++ sort_algorithm/smallest_diff_pair.py
@@ -3,7 +3,21 @@ def smallest_diff_pair(a, b):
     b.sort()
 
     i=j=0
+    
+    result = 99999
+    while i < len(a) and j < len(b):
+        if abs(a[i] - b[j]) < result:
+            result = abs(a[i] - b[j])
 
+        if a[i] < b[j]:
+            i += 1
+        else:
+            j += 1
+
+    return result
 
 a = [1, 2, 11, 5]
 b = [4, 12, 19, 23, 127, 235]
+
+print(smallest_diff_pair(a, b))
+
