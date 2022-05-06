3969333e4a07697ff90d916c3bfce981424cf43b
pbcquoc <pbcquoc@gmail.com>
2022-05-06 22:13:58 +0700

    bfs

---
+++ graph_algorithm/bfs.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+class Graph():
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def BFS(self, s):
+        visited = [False]*(max(self.graph) + 1)
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end=' ')
+
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)        
+g.BFS(2)
+    
